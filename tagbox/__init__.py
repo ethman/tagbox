44eb9f42a02f245dcd4cd5e0eee47e6951b192e5
Ethan Manilow <eth@nmanilow.com>
2021-10-23 15:58:47 -0500

    Exposing setup_jbx().

--- tagbox/__init__.py
+++ tagbox/__init__.py
@@ -1,6 +1,6 @@
 
 __version__ = '0.0.1'
 
-from .utils import load_audio_for_jbx, audio_for_jbx, make_labels, to_np
+from .utils import load_audio_for_jbx, audio_for_jbx, make_labels, to_np, setup_jbx
 from .utils import TAGGER_SR, JUKEBOX_SAMPLE_RATE
 from .runners import run_tagbox
